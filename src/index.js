acdfd0ea3171d172d82a5152b8b1c88fa04cdee6
James Young <jamthoyoung@gmail.com>
2017-01-06 12:16:37 -0500

    Modify css.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+//import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import kindredlist from './kindred.js';
