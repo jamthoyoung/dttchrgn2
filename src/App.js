c6cdd132996744ca95af6e9502875d6315f76821
James Young <jamthoyoung@gmail.com>
2017-01-06 15:43:13 -0500

    Start making use of bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import './App.css';
+import { Grid, Well, PageHeader, Jumbotron } from 'react-bootstrap';
 import CharacterBox from './component/CharacterBox';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-         <CharacterBox initFormData={this.props.initFormData} kindredlist={this.props.kindredlist}/>
-        </p>
-      </div>
+      <Grid fluid="true">
+        <PageHeader>
+          dttchrgen2
+        </PageHeader>
+        <Jumbotron>
+          <Well>
+            <CharacterBox initFormData={this.props.initFormData} kindredlist={this.props.kindredlist}/>
+          </Well>
+        </Jumbotron>
+      </Grid>
     );
   }
 }
