f8deafd13c52c60aa29459fc21b608fd948dbed6
James Young <jamthoyoung@gmail.com>
2017-01-13 12:40:54 -0500

    Clean up warnings.

--- src/component/OtherAbilities.js
+++ src/component/OtherAbilities.js
@@ -3,20 +3,22 @@ import { Row, Col } from 'react-bootstrap';
 
 class OtherAbilities extends React.Component {
   render(){
-    var attnames = ['str','con','dex','spd','lk','iq','wiz','chr'];
-    //var output = this.props.attr.str;
-    //var output = this.props.attr[attnames[0]];
     var name;
-    var output = '';
+    var output = [];
     for (name in this.props.attr){
+      if(this.props.attr.hasOwnProperty(name)){
        var att = this.props.attr[name];
        if(att.length > 3) {
-         output = output + "Specialized " + name.toUpperCase();
+         output.push("Specialized " + name.toUpperCase());
        } 
+      }
     }
     return (
-      <div>{output.toString()}
-      </div>
+      <Row>
+      {output.map(function(message){
+       return <Col sm={12}>{message}</Col>;
+      })}
+      </Row>
     );
   }
 }
