b88078653c3acecaef28cf3252682aa3ab8314bf
James Young <jamthoyoung@gmail.com>
2017-01-15 00:45:25 -0500

    New

---
+++ src/profession.js
@@ -0,0 +1,19 @@
+var backstory = [
+ {
+  "id":0,
+  "name":"None",
+  "talents":[]
+ },
+ {
+  "id":1,
+  "name":"Blacksmith",
+  "talents":["Blacksmith","Bargaining","Swordsmith"]
+ },
+ {
+  "id":2,
+  "name":"Donkey Rustler",
+  "talents":["Stealth","Donkey riding","Thievery"]
+ }
+];
+
+export default backstory;
