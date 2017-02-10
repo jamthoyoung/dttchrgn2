5cad648c5f5816b36ded0abe8ee6b392f8108ef4
James Young <jamthoyoung@gmail.com>
2017-02-10 15:23:11 -0500

    Add talentlist reducer in combine.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import character from './character';
 import kindredlist from './kindredlist';
+import talentlist from './talentlist';
 
 const characterApp = combineReducers({
  character,
- kindredlist
+ kindredlist,
+ talentlist
 });
 
 export default characterApp;
