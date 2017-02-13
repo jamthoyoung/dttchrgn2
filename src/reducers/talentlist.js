9ed22fd07784f7a6c63da24cb88614e8c534f516
James Young <jamthoyoung@gmail.com>
2017-02-13 14:38:50 -0500

    Add some talents.

--- src/reducers/talentlist.js
+++ src/reducers/talentlist.js
@@ -3,19 +3,35 @@ const initialState =
  byId : {
   1 : {
         id: 1,
-        name: "Blacksmith"
+        name: "Butcher"
   },
   2 : {
         id: 2,
-        name: "Jeweller"
+        name: "Surgeon"
   },
   3 : {
-        id: 2,
-        name: "Lockpicking"
+        id: 3,
+        name: "Anatomy"
+  },
+  4 : {
+        id: 4,
+        name: "Animal Husbandry"
+  },
+  5 : {
+        id: 5,
+        name: "Horsemanship or Riding"
+  },
+  6 : {
+        id: 6,
+        name: "Hunting and Tracking"
+  },
+  7 : {
+        id: 7,
+        name: "Animal Trainer"
   }
  },
- allIds : [ 1, 2, 3 ],
- regularIds : [ 1, 2 ],
+ allIds : [ 1, 2, 3, 4, 5 ],
+ regularIds : [ 1, 2, 3, 4, 5 ],
  rogueIds : [ 3 ]
 }
 
