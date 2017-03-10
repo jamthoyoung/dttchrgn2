0a4cb49f26d62699f2aadbf266b5353c3e5e5154
James Young <jamthoyoung@gmail.com>
2017-03-09 23:06:36 -0500

    Remove unused imports.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { PageHeader } from 'react-bootstrap';
 import CharacterBox from './component/CharacterBox';
 
-import initFormData from './initFormData.js';
-import backstory from './backstory.js';
-
 class App extends Component {
   render() {
     return (
