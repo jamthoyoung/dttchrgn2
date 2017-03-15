3b1041083573bc5b888f038db2b9268d2eb40b6f
James Young <jamthoyoung@gmail.com>
2017-03-15 19:22:24 -0400

    Update css

--- src/component/PrintScreen.js
+++ src/component/PrintScreen.js
@@ -4,8 +4,10 @@ import { Image } from 'react-bootstrap';
 class PrintScreen extends React.Component {
   render(){
     return (
-      <div>
-        <Image id="section-to-print" src="/assets/tt-character-sheet.jpg" responsive />
+      <div id="section-to-print">
+        <Image id="image" src="/assets/tt-character-sheet.jpg" responsive />
+        <div id="str-to-print" class="field-to-print">32</div>
+        <div id="iq-to-print" class="field-to-print">99</div>
       </div>
     );
   }
