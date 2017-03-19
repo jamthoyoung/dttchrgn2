ccd5e9cdaecdd6de40f8927b5f8aea8382b9a140
James Young <jamthoyoung@gmail.com>
2017-03-19 11:48:29 -0400

    Set defaults to 0

--- src/reducers/character.js
+++ src/reducers/character.js
@@ -1,6 +1,6 @@
 import { SELECT_TALENTS, SELECT_KINDRED, REROLL_DICE, UPDATE_NAME, SELECT_GENDER, SELECT_CLASS } from '../constants/ActionTypes';
 
-const initialState = 
+const initialState =
 {
   "name" : "Enter name",
   "gender" : "Male",
@@ -11,19 +11,19 @@ const initialState =
   },
   "talents":[ 1, 3],
   "attributes" : {
-    str : [ 1, 1, 2 ],
-    con : [ 1, 1, 2 ],
-    dex : [ 1, 1, 2 ],
-    spd : [ 1, 1, 2 ],
-    lk : [ 1, 1, 2 ],
-    iq : [ 1, 1, 2 ],
-    wiz : [ 1, 1, 2 ],
-    chr : [ 1, 1, 2 ]
+    str : [ -1, 0, 1 ],
+    con : [ -1, 0, 1 ],
+    dex : [ -1, 0, 1 ],
+    spd : [ -1, 0, 1 ],
+    lk : [ -1, 0, 1 ],
+    iq : [ -1, 0, 1 ],
+    wiz : [ -1, 0, 1 ],
+    chr : [ -1, 0, 1 ]
   },
   class: "Warrior",
-  height: 11,
-  weight: 12,
-  gold: 30,
+  height: 3,
+  weight: 3,
+  gold: 0,
   equipment : [
     { name: "Short Sword",
       damagedice: 3,
@@ -41,7 +41,7 @@ const initialState =
       damageadds: 0,
       weight: 25,
       active: false
-    } 
+    }
   ]
 };
 
