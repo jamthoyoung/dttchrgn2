657149a367c356f2f014f8906e2c8e52b613c49a
James Young <jamthoyoung@gmail.com>
2017-04-16 15:58:14 -0400

    Add rest of talents.

--- src/reducers/talentlist.js
+++ src/reducers/talentlist.js
@@ -477,6 +477,176 @@ const initialState =
         id: 95,
         group: 20,
         name: "Theoretical math"
+  },
+  96 : {
+        id: 96,
+        group: 21,
+        name: "Clockworks"
+  },
+  97 : {
+        id: 97,
+        group: 21,
+        name: "Siege machinery"
+  },
+  98 : {
+        id: 98,
+        group: 22,
+        name: "Military tactics"
+  },
+  99 : {
+        id: 99,
+        group: 22,
+        name: "Small unit tactics"
+  },
+  100 : {
+        id: 100,
+        group: 23,
+        name: "Competetive drinking"
+  },
+  101 : {
+        id: 101,
+        group: 23,
+        name: "Endurance"
+  },
+  102 : {
+        id: 102,
+        group: 23,
+        name: "Self-defence"
+  },
+  103 : {
+        id: 103,
+        group: 23,
+        name: "Dodge"
+  },
+  104 : {
+        id: 104,
+        group: 24,
+        name: "Anatomy"
+  },
+  105 : {
+        id: 105,
+        group: 24,
+        name: "Chemistry"
+  },
+  106 : {
+        id: 106,
+        group: 24,
+        name: "Medicine"
+  },
+  107 : {
+        id: 107,
+        group: 24,
+        name: "Research skills"
+  },
+  108 : {
+        id: 108,
+        group: 25,
+        name: "Arson"
+  },
+  109 : {
+        id: 109,
+        group: 25,
+        name: "Assassin"
+  },
+  110 : {
+        id: 110,
+        group: 25,
+        name: "Conspiracy"
+  },
+  111 : {
+        id: 111,
+        group: 25,
+        name: "Disguise"
+  },
+  112 : {
+        id: 112,
+        group: 25,
+        name: "Gambling"
+  },
+  113 : {
+        id: 113,
+        group: 25,
+        name: "Garrote"
+  },
+  114 : {
+        id: 114,
+        group: 25,
+        name: "Lock-picking"
+  },
+  115 : {
+        id: 115,
+        group: 25,
+        name: "Pickpocket"
+  },
+  116 : {
+        id: 116,
+        group: 25,
+        name: "Poisoner"
+  },
+  117 : {
+        id: 117,
+        group: 25,
+        name: "Sleight of hand"
+  },
+  118 : {
+        id: 118,
+        group: 25,
+        name: "Streetwise"
+  },
+  119 : {
+        id: 119,
+        group: 25,
+        name: "Spy"
+  },
+  120 : {
+        id: 120,
+        group: 25,
+        name: "Stealth"
+  },
+  121 : {
+        id: 121,
+        group: 25,
+        name: "Thievery"
+  },
+  122 : {
+        id: 122,
+        group: 26,
+        name: "Camping"
+  },
+  123 : {
+        id: 123,
+        group: 26,
+        name: "Firemaking"
+  },
+  124 : {
+        id: 124,
+        group: 26,
+        name: "Fishing"
+  },
+  125 : {
+        id: 125,
+        group: 26,
+        name: "Hunting"
+  },
+  126 : {
+        id: 126,
+        group: 26,
+        name: "Knot tying"
+  },
+  127 : {
+        id: 127,
+        group: 26,
+        name: "Overland trekking"
+  },
+  128 : {
+        id: 128,
+        group: 26,
+        name: "Trapping"
+  },
+  129 : {
+        id: 129,
+        group: 26,
+        name: "Wilderness survival"
   }
  },
  groups: {
@@ -582,32 +752,32 @@ const initialState =
    },
    21: {
      id: 21,
-     talentIds: [],
+     talentIds: [96, 97],
      name: "Mechanics"
    },
    22: {
      id: 22,
-     talentIds: [],
+     talentIds: [98, 99],
      name: "Leadership"
    },
    23: {
      id: 23,
-     talentIds: [],
+     talentIds: [100, 101, 102, 103],
      name: "Resilience"
    },
    24: {
      id: 24,
-     talentIds: [],
+     talentIds: [104, 105, 106, 107],
      name: "Scientist, Investigator"
    },
    25: {
      id: 25,
-     talentIds: [],
+     talentIds: [108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121],
      name: "Subtlety, Roguery, Thievery"
    },
    26: {
      id: 26,
-     talentIds: [],
+     talentIds: [122, 123, 124, 125, 126, 127, 128, 129],
      name: "Wilderness Skills, Outdoormanship"
    }
  },
@@ -615,12 +785,14 @@ const initialState =
  allIds : [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
             30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
             58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
-            86, 87, 88, 89, 90, 91, 92, 93, 94, 95],
+            86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
+            111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129],
  regularIds : [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29,
             30, 31, 32, 33, 34, 35, 36, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
             59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
-            87, 88, 89, 90, 91, 92, 93, 94, 95],
- rogueIds : [ 22, 37 ]
+            87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 104, 105, 106, 107, 122, 123, 124, 125, 126,
+            127, 128, 129],
+ rogueIds : [ 22, 37, 103, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121 ]
 }
 
 export default function talentlist(state = initialState, action){
