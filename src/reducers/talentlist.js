9be5641843b13e2c6da4f9d78949b50e148bd1ac
James Young <jamthoyoung@gmail.com>
2017-04-16 01:38:28 -0400

    Update list of talents.

--- src/reducers/talentlist.js
+++ src/reducers/talentlist.js
@@ -97,6 +97,91 @@ const initialState =
         id: 19,
         group: 4,
         name: "Contortionist"
+  },
+  20 : {
+        id: 20,
+        group: 4,
+        name: "Dance (or specific dances)"
+  },
+  21 : {
+        id: 21,
+        group: 4,
+        name: "Deep water diving"
+  },
+  22 : {
+        id: 22,
+        group: 4,
+        name: "Acrobat"
+  },
+  23 : {
+        id: 23,
+        group: 4,
+        name: "Juggler"
+  },
+  24 : {
+        id: 24,
+        group: 4,
+        name: "Martial Artist"
+  },
+  25 : {
+        id: 25,
+        group: 4,
+        name: "Mountaineer"
+  },
+  26 : {
+        id: 26,
+        group: 4,
+        name: "Skier"
+  },
+  27 : {
+        id: 27,
+        group: 4,
+        name: "Caver"
+  },
+  28 : {
+        id: 28,
+        group: 4,
+        name: "Swimmer"
+  },
+  29 : {
+        id: 29,
+        group: 5,
+        name: "Apothecary"
+  },
+  30 : {
+        id: 30,
+        group: 5,
+        name: "Healer"
+  },
+  31 : {
+        id: 31,
+        group: 5,
+        name: "Herbalist"
+  },
+  32 : {
+        id: 32,
+        group: 5,
+        name: "Poisoner"
+  },
+  33 : {
+        id: 33,
+        group: 6,
+        name: "Architect"
+  },
+  34 : {
+        id: 34,
+        group: 6,
+        name: "Engineer"
+  },
+  35 : {
+        id: 35,
+        group: 6,
+        name: "Mason"
+  },
+  36 : {
+        id: 36,
+        group: 6,
+        name: "Shipbuilder"
   }
  },
  groups: {
@@ -117,14 +202,26 @@ const initialState =
    },
    4: {
      id: 4,
-     talentIds: [19],
+     talentIds: [19, 20, 21, 22, 23, 24, 25, 26, 27, 28],
      name: "Athlete"
    },
+   5: {
+     id: 5,
+     talentIds: [29, 30, 31, 32],
+     name: "Botany, Knowledge of Plants"
+   },
+   6: {
+     id: 6,
+     talentIds: [33, 34, 35, 36],
+     name: "Botany, Knowledge of Plants"
+   }
  },
- allGroups: [ 1, 2, 3, 4],
- allIds : [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ],
- regularIds : [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ],
- rogueIds : [ 3 ]
+ allGroups: [ 1, 2, 3, 4, 5, 6],
+ allIds : [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32, 33, 34, 35, 36],
+ regularIds : [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32, 33, 34, 35, 36],
+ rogueIds : [ 22 ]
 }
 
 export default function talentlist(state = initialState, action){
